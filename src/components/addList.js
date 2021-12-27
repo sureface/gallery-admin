01a2add5b5a254b081803212fe96c7db9d5bab28
sureface <jasurdjuraev2002@gmail.com>
2021-12-27 20:19:48 +0300

    done

--- src/components/addList.js
+++ src/components/addList.js
@@ -1,24 +1,54 @@
 import React, {useState} from 'react';
-import {useForm} from "react-hook-form";
 import NavigationBar from "./navigationBar";
 import {Link} from "react-router-dom";
 import {Breadcrumb, BreadcrumbItem} from "reactstrap";
-
+import axios from "axios";
 
 
 const AddList = () => {
 
 
-    const onChangeInput = (e) => {
-        let file = e.target.files;
+    const [albumName, setAlbumName] = useState("");
+    const [albumDescription, setAlbumDescription] = useState("");
+
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+
+        let thumbnail = e.target[0].files[0];
+        let imagesFiles = e.target[3].files;
 
         let formData = new FormData();
 
-        formData.append("files[]", file[0]);
+        formData.append("files[]", thumbnail);
 
-        console.warn("******", ...formData);
+        for (let item of imagesFiles) {
+            formData.append("files[]", item);
+        }
+
+        axios.post(`${process.env["REACT_APP_API"]}/api/cockpit/addAssets?token=73ad18f6896b8a47f97bfe3f824958`, formData)
+            .then((res) => {
+                let assets = res.data.assets;
+                let thumbnailPath = "storage/uploads/" + assets[0].path;
+                let imagesPath = [];
+
+                for (let i = 1; i < assets.length; i++) {
+                    imagesPath.push({path: "storage/uploads/" + assets[i].path})
+                }
+
+                return axios.post(`${process.env["REACT_APP_API"]}/api/collections/save/album?token=73ad18f6896b8a47f97bfe3f824958`, {
+                    data: {
+                        Description: albumDescription,
+                        Heading: albumName,
+                        Images: imagesPath,
+                        Thumbnail: {path: thumbnailPath}
+                    }
+                }, {headers: {'Content-Type': 'application/json'}})
+                    .then((res) => {
+                        console.log(res);
+                    })
+                    .catch(err => console.log(err));
+            });
     }
-
 
     return (
         <div className="addList">
@@ -26,7 +56,8 @@ const AddList = () => {
             <div className="container">
                 <div className="my-3">
                     <Breadcrumb>
-                        <BreadcrumbItem><Link to="/" className="text-primary text-decoration-none">Dashboard</Link></BreadcrumbItem>
+                        <BreadcrumbItem><Link to="/"
+                                              className="text-primary text-decoration-none">Dashboard</Link></BreadcrumbItem>
                         <BreadcrumbItem active>List</BreadcrumbItem>
                     </Breadcrumb>
                 </div>
@@ -34,19 +65,20 @@ const AddList = () => {
             <div className="container">
 
                 <div className="my-5">
-                    <form className="form-wrapper">
+                    <form className="form-wrapper" onSubmit={onSubmitHandler}>
                         <div className="form-group">
                             <label className="mb-2" htmlFor="upload">Album overview image</label>
-                            <input className="form-control mb-3" type="file" name="upload" id="upload" onChange={onChangeInput} />
+                            <input className="form-control mb-3" type="file" name="upload" id="upload"/>
                         </div>
                         <div className="form-group">
                             <label className="mb-2" htmlFor="title">Album Name</label>
                             <input type="text" className="form-control mb-3" placeholder="Album Name" id="title"
-                                   name="title" required/>
+                                   name="title" required onChange={(e) => setAlbumName(e.target.value)}/>
                         </div>
                         <div className="form-group">
                             <label className="mb-2" htmlFor="subtitle">Album description</label>
-                            <textarea className="form-control mb-3" id="subtitle" placeholder="Album description"/>
+                            <textarea className="form-control mb-3" id="subtitle" placeholder="Album description"
+                                      onChange={(e) => setAlbumDescription(e.target.value)}/>
                         </div>
                         <div className="form-group">
                             <label className="mb-2" htmlFor="uploadAlbum">Albums</label>
